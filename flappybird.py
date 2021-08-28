ad8f807aa9c5fded7e87a74827239651979bf21d
Zainab Lawal <zainablaw1012@gmail.com>
2021-08-28 18:44:59 +0300

    Added the actors

--- flappybird.py
+++ flappybird.py
@@ -1,12 +1,26 @@
 import pgzrun
 
 TITLE = 'Flappy Bird'
-WIDTH = 400
-HEIGHT = 700
+WIDTH = 250
+HEIGHT = 400
 
 def draw():
-    screen.blit('cascade', (0,0))
+    screen.blit('bg', (0,0))
+    bird.draw()
+    top_pipe.draw()
+    bottom_pipe.draw()
 
 
+def on_mouse_down():
+    print('The mouse was clicked')
+    bird.y -= 50
+
+def update():
+    bird.y += bird.speed
+
+bird = Actor('bird', (75,130))
+bird.speed = 1
+top_pipe = Actor('pipe-upper',(150,0))
+bottom_pipe = Actor('pipe-lower',(150,250))
 
 pgzrun.go()
